0ecdbdf89281454338f0c4f7dc877cf7249a7671
rstata <rstata@yahoo.com>
2017-06-27 21:24:18 -0400

    Evolved tests

--- lang/java/avro/src/main/java/org/apache/avro/io/parquet/ParquetEncoderTest.java
+++ lang/java/avro/src/main/java/org/apache/avro/io/parquet/ParquetEncoderTest.java
@@ -25,10 +25,10 @@ import org.apache.hadoop.fs.Path;
 // import org.apache.parquet.schema.GroupType;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.PrimitiveType;
-// import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY;
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT64;
-// import static org.apache.parquet.schema.Type.Repetition.OPTIONAL;
-// import static org.apache.parquet.schema.Type.Repetition.REPEATED;
+import static org.apache.parquet.schema.Type.Repetition.OPTIONAL;
+import static org.apache.parquet.schema.Type.Repetition.REPEATED;
 import static org.apache.parquet.schema.Type.Repetition.REQUIRED;
 
 import org.apache.parquet.column.ParquetProperties;
@@ -41,12 +41,41 @@ class ParquetEncoderTest {
 
     ParquetProperties props = ParquetProperties.builder().build();
 
-    // Test 1
-    ParquetEncoder e = new ParquetEncoder(new Path("t1"), t1, props);
+    test1(props, t1);
+    test1(props, t2);
+    test1(props, t3);
+    test1(props, t4);
+  }
+
+  public static void test1(ParquetProperties p, MessageType t)
+    throws IOException
+  {
+    Path f = new Path(t.getName());
+    ParquetEncoder e = new ParquetEncoder(f, t, p);
+    e.close();
   }
 
   public static MessageType t1 =
-    new MessageType("Document",
+    new MessageType("t1",
       new PrimitiveType(REQUIRED, INT64, "DocId")
+    );
+
+  public static MessageType t2 =
+    new MessageType("t2",
+      new PrimitiveType(REQUIRED, INT64, "DocId"),
+      new PrimitiveType(REQUIRED, BINARY, "DocName")
+    );
+
+  public static MessageType t3 =
+    new MessageType("t3",
+      new PrimitiveType(REQUIRED, INT64, "DocId"),
+      new PrimitiveType(OPTIONAL, BINARY, "DocName")
+    );
+
+  public static MessageType t4 =
+    new MessageType("t4",
+      new PrimitiveType(REQUIRED, INT64, "DocId"),
+      new PrimitiveType(REQUIRED, BINARY, "DocName"),
+      new PrimitiveType(REPEATED, BINARY, "DocAlias")
     );
 }
