3fd2674ff37e9ae27daf58e0cb5301764ff1a6b1
rstata <rstata@yahoo.com>
2017-06-30 05:11:49 -0700

    Eliminated unneeded dependency on Avro Encoder interface

--- lang/java/avro/src/main/java/org/apache/avro/io/parquet/ParquetEncoder.java
+++ lang/java/avro/src/main/java/org/apache/avro/io/parquet/ParquetEncoder.java
@@ -22,14 +22,12 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import org.apache.avro.AvroTypeException;
-import org.apache.avro.io.Encoder;
 import org.apache.avro.io.parsing.Parser;
 import org.apache.avro.io.parsing.ParquetGrammar.ArrayRepLevel;
 import org.apache.avro.io.parsing.ParquetGrammar.FieldWriteAction;
 import org.apache.avro.io.parsing.ParquetGrammar.FixedWriteAction;
 import org.apache.avro.io.parsing.ParquetGrammar.WriteNullsAction;
 import org.apache.avro.io.parsing.Symbol;
-import org.apache.avro.util.Utf8;
 
 import org.apache.parquet.column.ColumnWriter;
 import org.apache.parquet.column.ParquetProperties;
@@ -39,7 +37,7 @@ import org.apache.parquet.schema.MessageType;
 import org.apache.hadoop.fs.Path;
 
 
-public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
+public class ParquetEncoder implements Parser.ActionHandler {
   private final ParquetEncoderWriter writer;
 
   private Parser parser;
@@ -70,7 +68,6 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
   }
 
   /** Can only flush on row boundaries!! */
-  @Override
   public void flush() throws IOException {
     if (parser.topSymbol() != Symbol.RECORD_END) {
       throw new IllegalStateException("Attempt to flush before record ended.");
@@ -109,56 +106,48 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
    * no startItems were called, then the array is empty, and we need
    * to arrange to wite out nulls for any leaves in this array). */
 
-  @Override
   public void writeBoolean(boolean b) throws IOException {
     parser.advance(Symbol.BOOLEAN);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(b, repLevel, top.defLevel);
   }
 
-  @Override
   public void writeInt(int n) throws IOException {
     parser.advance(Symbol.INT);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(n, repLevel, top.defLevel);
   }
 
-  @Override
   public void writeLong(long n) throws IOException {
     parser.advance(Symbol.LONG);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(n, repLevel, top.defLevel);
   }
 
-  @Override
   public void writeFloat(float f) throws IOException {
     parser.advance(Symbol.FLOAT);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(f, repLevel, top.defLevel);
   }
 
-  @Override
   public void writeDouble(double d) throws IOException {
     parser.advance(Symbol.DOUBLE);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(d, repLevel, top.defLevel);
   }
 
-  @Override
   public void writeBytes(ByteBuffer bytes) throws IOException {
     parser.advance(Symbol.BYTES);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(Binary.fromReusedByteBuffer(bytes), repLevel, top.defLevel);
   }
 
-  @Override
   public void writeBytes(byte[] b, int s, int l) throws IOException {
     parser.advance(Symbol.BYTES);
     FieldWriteAction top = (FieldWriteAction) parser.popSymbol();
     top.col.write(Binary.fromReusedByteArray(b, s, l), repLevel, top.defLevel);
   }
 
-  @Override
   public void writeFixed(byte[] b, int s, int l) throws IOException {
     parser.advance(Symbol.FIXED);
     FixedWriteAction top = (FixedWriteAction) parser.popSymbol();
@@ -170,7 +159,6 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
     top.col.write(Binary.fromReusedByteArray(b, s, l), repLevel, top.defLevel);
   }
 
-  @Override
   public void writeIndex(int unionIndex) throws IOException {
     parser.advance(Symbol.UNION);
     Symbol.Alternative top = (Symbol.Alternative) parser.popSymbol();
@@ -199,7 +187,6 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
   private RepeaterState[] rstates = new RepeaterState[10];
   private int pos = -1;
 
-  @Override
   public void writeArrayStart() throws IOException {
     parser.advance(Symbol.ARRAY_START);
     ArrayRepLevel top = (ArrayRepLevel) parser.popSymbol();
@@ -215,7 +202,6 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
     nextItemIndex = 0;
   }
 
-  @Override
   public void startItem() throws IOException {
     if (nextItemIndex++ == 1) {
       // After we've successfully written item #0, update the
@@ -225,7 +211,6 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
     }
   }
 
-  @Override
   public void writeArrayEnd() throws IOException {
     parser.advance(Symbol.ARRAY_END);
     WriteNullsAction top = (WriteNullsAction) parser.popSymbol();
@@ -244,7 +229,6 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
   }
 
 
-  @Override
   public Symbol doAction(Symbol input, Symbol top) throws IOException {
     if (top == Symbol.RECORD_END) {
       writer.endRecord();
@@ -252,39 +236,5 @@ public class ParquetEncoder extends Encoder implements Parser.ActionHandler {
       throw new IllegalStateException("Unknown action symbol " + top);
     }
     return null;
-  }
-
-
-  // These methods aren't relevant in the context of our Parquet writer.
-  // This is a symptom of reusing an Avro implementation for Parquet.
-
-  @Override
-  public void writeString(Utf8 utf8) throws IOException {
-    throw new UnsupportedOperationException("Use writeBytes instead.");
-  }
-
-  @Override
-  public void writeNull() throws IOException {
-    throw new UnsupportedOperationException("Use writeIndex instead.");
-  }
-
-  @Override
-  public void writeEnum(int e) throws IOException {
-    throw new UnsupportedOperationException("Not supported in Parquet.");
-  }
-
-  @Override
-  public void setItemCount(long c) throws IOException {
-    throw new UnsupportedOperationException("Not supported in Parquet.");
-  }
-
-  @Override
-  public void writeMapStart() throws IOException {
-    throw new UnsupportedOperationException("Not supported in Parquet.");
-  }
-
-  @Override
-  public void writeMapEnd() throws IOException {
-    throw new UnsupportedOperationException("Not supported in Parquet.");
   }
 }
