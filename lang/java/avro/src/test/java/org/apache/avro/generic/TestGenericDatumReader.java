1b1d1fbedddeb205154350ce397dd89a166869cb
Thiruvalluvan M G <thiru@startsmartlabs.com>
2019-05-03 11:07:18 -0700

    Code compiles, again

--- lang/java/avro/src/test/java/org/apache/avro/generic/TestGenericDatumReader.java
+++ lang/java/avro/src/test/java/org/apache/avro/generic/TestGenericDatumReader.java
@@ -1,6 +1,5 @@
 package org.apache.avro.generic;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.Schema;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
@@ -31,8 +30,8 @@ public class TestGenericDatumReader {
   private final Object input;
   private final Object expectedOutput;
 
-  public TestGenericDatumReader(String description, Schema writerSchema, Schema readerSchema,
-                                Object input, Object expectedOutput) {
+  public TestGenericDatumReader(String description, Schema writerSchema, Schema readerSchema, Object input,
+      Object expectedOutput) {
     this.description = description;
     this.writerSchema = writerSchema;
     this.readerSchema = readerSchema;
@@ -62,7 +61,7 @@ public class TestGenericDatumReader {
     for (Object[] row : testData) {
       Schema wp = new Schema.Parser().parse((String) (row[1]));
       Schema rp = new Schema.Parser().parse((String) (row[2]));
-      result.add(new Object[]{row[0], wp, rp, row[3], row[4]});
+      result.add(new Object[] { row[0], wp, rp, row[3], row[4] });
     }
     return result;
   }
@@ -71,58 +70,70 @@ public class TestGenericDatumReader {
   private static GenericData GENERIC_DATA = new GenericData();
 
   static {
-    testData = new Object[][]{
-      // Identical reader and writer schemas
-        {"plain null", "{\"type\": \"null\"}", "{\"type\": \"null\"}", null, null},
-        {"plain boolean", "{\"type\": \"boolean\"}", "{\"type\": \"boolean\"}", true, true},
-        {"plain int", "{\"type\": \"int\"}", "{\"type\": \"int\"}", 10, 10},
-        {"plain long", "{\"type\": \"long\"}", "{\"type\": \"long\"}", 100L, 100L},
-        {"plain float", "{\"type\": \"float\"}", "{\"type\": \"float\"}", 1000.0f, 1000.0f},
-        {"plain double", "{\"type\": \"double\"}", "{\"type\": \"double\"}", 10000.0, 10000.0},
-        {"plain string", "{\"type\": \"string\"}", "{\"type\": \"string\"}", "\"s\"", "\"s\""},
-        {"plain bytes", "{\"type\": \"bytes\"}", "{\"type\": \"bytes\"}", ByteBuffer.wrap(new byte[]{0, 2, 4, 6}),
-          ByteBuffer.wrap(new byte[]{0, 2, 4, 6})},
-        {"plain fixed(4)", "{\"name\": \"fxd\", \"type\": \"fixed\", \"size\": 4}",
-          "{\"name\": \"fxd\", \"type\": \"fixed\", \"size\": 4}",
-          GENERIC_DATA.createFixed(null, new byte[]{0, 3, 6, 9}, Schema.createFixed("fxd", "", "", 4)),
-          GENERIC_DATA.createFixed(null, new byte[]{0, 3, 6, 9}, Schema.createFixed("fxd", "", "", 4))},
-        {"plain union<int, string>", "[\"int\", \"string\"]", "[\"int\", \"string\"]", "\"s\"", "\"s\""},
-        {"plain array<int>", "{\"type\": \"array\", \"items\": \"int\"}", "{\"type\": \"array\", \"items\": \"int\"}",
-          Arrays.asList(10, 20, 30), Arrays.asList(10, 20, 30)},
-        {"plain map<string, int>", "{\"type\": \"map\", \"values\": \"int\"}", "{\"type\": \"map\", \"values\": \"int\"}",
-          new HashMap<String, Integer>() {{put("a", 1); put("b", 2);}},
-          new HashMap<String, Integer>() {{put("a", 1); put("b", 2);}}},
+    testData = new Object[][] {
+        // Identical reader and writer schemas
+        { "plain null", "{\"type\": \"null\"}", "{\"type\": \"null\"}", null, null },
+        { "plain boolean", "{\"type\": \"boolean\"}", "{\"type\": \"boolean\"}", true, true },
+        { "plain int", "{\"type\": \"int\"}", "{\"type\": \"int\"}", 10, 10 },
+        { "plain long", "{\"type\": \"long\"}", "{\"type\": \"long\"}", 100L, 100L },
+        { "plain float", "{\"type\": \"float\"}", "{\"type\": \"float\"}", 1000.0f, 1000.0f },
+        { "plain double", "{\"type\": \"double\"}", "{\"type\": \"double\"}", 10000.0, 10000.0 },
+        { "plain string", "{\"type\": \"string\"}", "{\"type\": \"string\"}", "\"s\"", "\"s\"" },
+        { "plain bytes", "{\"type\": \"bytes\"}", "{\"type\": \"bytes\"}", ByteBuffer.wrap(new byte[] { 0, 2, 4, 6 }),
+            ByteBuffer.wrap(new byte[] { 0, 2, 4, 6 }) },
+        { "plain fixed(4)", "{\"name\": \"fxd\", \"type\": \"fixed\", \"size\": 4}",
+            "{\"name\": \"fxd\", \"type\": \"fixed\", \"size\": 4}",
+            GENERIC_DATA.createFixed(null, new byte[] { 0, 3, 6, 9 }, Schema.createFixed("fxd", "", "", 4)),
+            GENERIC_DATA.createFixed(null, new byte[] { 0, 3, 6, 9 }, Schema.createFixed("fxd", "", "", 4)) },
+        { "plain union<int, string>", "[\"int\", \"string\"]", "[\"int\", \"string\"]", "\"s\"", "\"s\"" },
+        { "plain array<int>", "{\"type\": \"array\", \"items\": \"int\"}", "{\"type\": \"array\", \"items\": \"int\"}",
+            Arrays.asList(10, 20, 30), Arrays.asList(10, 20, 30) },
+        { "plain map<string, int>", "{\"type\": \"map\", \"values\": \"int\"}",
+            "{\"type\": \"map\", \"values\": \"int\"}", new HashMap<String, Integer>() {
+              {
+                put("a", 1);
+                put("b", 2);
+              }
+            }, new HashMap<String, Integer>() {
+              {
+                put("a", 1);
+                put("b", 2);
+              }
+            } },
 //      FIXME: Enum is not handled yet
-        {"plain enum(a, b)", "{\"name\": \"enm\", \"type\": \"enum\", \"symbols\": [\"A\", \"B\"]}",
-          "{\"name\": \"enm\", \"type\": \"enum\", \"symbols\": [\"A\", \"B\"]}",
-          GENERIC_DATA.createEnum("A", Schema.createEnum("enm", "", null, Arrays.asList("A", "B"))),
-          GENERIC_DATA.createEnum("A", Schema.createEnum("enm", "", null, Arrays.asList("A", "B")))},
-      //{"plain record", "{\"type"\"}"}
+        { "plain enum(a, b)", "{\"name\": \"enm\", \"type\": \"enum\", \"symbols\": [\"A\", \"B\"]}",
+            "{\"name\": \"enm\", \"type\": \"enum\", \"symbols\": [\"A\", \"B\"]}",
+            GENERIC_DATA.createEnum("A", Schema.createEnum("enm", "", null, Arrays.asList("A", "B"))),
+            GENERIC_DATA.createEnum("A", Schema.createEnum("enm", "", null, Arrays.asList("A", "B"))) },
+        // {"plain record", "{\"type"\"}"}
 
-      // Nested types
+        // Nested types
 
-      // Promotions
-      {"int to long", "{\"type\": \"int\"}", "{\"type\": \"long\"}", 100, 100L},
-      {"int to float", "{\"type\": \"int\"}", "{\"type\": \"float\"}", 101, 101.0f},
-      {"int to double", "{\"type\": \"int\"}", "{\"type\": \"double\"}", 102, 102.0},
-      {"long to float", "{\"type\": \"long\"}", "{\"type\": \"float\"}", 103L, 103.0f},
-      {"long to double", "{\"type\": \"long\"}", "{\"type\": \"double\"}", 104L, 104.0},
-      {"float to double", "{\"type\": \"float\"}", "{\"type\": \"double\"}", 105.f, 105.0},
-      {"string to bytes", "{\"type\": \"string\"}", "{\"type\": \"bytes\"}", "a10", ByteBuffer.wrap(new byte[]{'a', '1', '0'})},
-      {"bytes to string", "{\"type\": \"bytes\"}", "{\"type\": \"string\"}", ByteBuffer.wrap(new byte[]{'a', '1', '0'}), "a10"},
+        // Promotions
+        { "int to long", "{\"type\": \"int\"}", "{\"type\": \"long\"}", 100, 100L },
+        { "int to float", "{\"type\": \"int\"}", "{\"type\": \"float\"}", 101, 101.0f },
+        { "int to double", "{\"type\": \"int\"}", "{\"type\": \"double\"}", 102, 102.0 },
+        { "long to float", "{\"type\": \"long\"}", "{\"type\": \"float\"}", 103L, 103.0f },
+        { "long to double", "{\"type\": \"long\"}", "{\"type\": \"double\"}", 104L, 104.0 },
+        { "float to double", "{\"type\": \"float\"}", "{\"type\": \"double\"}", 105.f, 105.0 },
+        { "string to bytes", "{\"type\": \"string\"}", "{\"type\": \"bytes\"}", "a10",
+            ByteBuffer.wrap(new byte[] { 'a', '1', '0' }) },
+        { "bytes to string", "{\"type\": \"bytes\"}", "{\"type\": \"string\"}",
+            ByteBuffer.wrap(new byte[] { 'a', '1', '0' }), "a10" },
 
-      // Promotion in array
+        // Promotion in array
 
-      // Promotion in map
+        // Promotion in map
 
-      // Promotion in record
+        // Promotion in record
 
-      {"union<int, string> to string", "[\"int\", \"string\"]", "{\"type\": \"string\"}", "s", "s"},
-      {"string to union<int, string>", "{\"type\": \"string\"}", "[\"int\", \"string\"]", "s", "s"},
+        { "union<int, string> to string", "[\"int\", \"string\"]", "{\"type\": \"string\"}", "s", "s" },
+        { "string to union<int, string>", "{\"type\": \"string\"}", "[\"int\", \"string\"]", "s", "s" },
 
-      // FIXME: some trouble with arrayIndex
-      {"union<int, string> to union<long, string, boolean>", "[\"int\", \"string\"]", "[\"long\", \"string\", \"boolean\"]", "s", "s"},
-      {"union<long, string, boolean> to union<int, string>", "[\"long\", \"string\", \"boolean\"]", "[\"int\", \"string\"]", "s", "s"},
-    };
+        // FIXME: some trouble with arrayIndex
+        { "union<int, string> to union<long, string, boolean>", "[\"int\", \"string\"]",
+            "[\"long\", \"string\", \"boolean\"]", "s", "s" },
+        { "union<long, string, boolean> to union<int, string>", "[\"long\", \"string\", \"boolean\"]",
+            "[\"int\", \"string\"]", "s", "s" }, };
   }
 }
